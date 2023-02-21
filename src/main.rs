1cbb2528a8739a0f77682072225dfa5a4ed372d4
Reguzzoni <a.reguzzoni@outlook.it>
2023-02-21 01:14:55 +0100

    first test pq

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use priority_queue::PriorityQueue;
+
+fn main() {
+  // test priority queue
+  priority_queue();
+}
+
+fn priority_queue() {
+  let mut q = PriorityQueue::new();
+
+  // Add elements to an empty queue:
+  assert!(q.is_empty());
+  q.push("Maths", 10);
+  q.push("Physics", 5);
+  q.push("Chemistry", 15);
+
+  // Check if Chemistry has the highest priority:
+  assert_eq!(q.peek(), Some((&"Chemistry", &15)));
+
+  // Give highest priority to Maths:
+  q.change_priority("Maths", 20);
+
+  // Check if Maths has the highest priority:
+  assert_eq!(q.peek(), Some((&"Maths", &20)));
+  println!("Tests passed!");
+}
